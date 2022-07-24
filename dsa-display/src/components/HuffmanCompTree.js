ae85d6e899e6c882a54ef7e59da83d25a23941b1
Johnathan <jocuog@gmail.com>
2022-07-24 11:07:34 -0600

    huffman tree html

--- dsa-display/src/components/HuffmanCompTree.js
+++ dsa-display/src/components/HuffmanCompTree.js
@@ -26,17 +26,36 @@ const HuffmanCompTree = () => {
         return lettersObj;
     }
 
-    const onButtonClick = () => {
-        console.log("click")
+    const handleOnChange = (e) => {
+        console.log(e.target.value)
+        const inputString = e.target.value
     }
 
     return (
         <div>
-            <h2>Enter String Here:</h2><br/>
-            <input type="text" id='input' value='Enter a string to encode'/>
-            <button onClick={onButtonClick}>Encode!</button><br/>
-            <h4>The Huffman encoded string is: <span id='output' style={{ fontFamily: 'monospace' }}></span></h4><br/>
-            
+            <h2>Huffman Code Compression Tree</h2><br/>
+
+            <input 
+                type="text" 
+                id='input' 
+                onChange={handleOnChange}
+                placeholder='Enter String to Encode' />
+
+            <button >Encode!</button><br/>
+
+            <h4>The Huffman encoded string is: 
+                <span id='output' style={{ fontFamily: 'monospace' }}></span>
+            </h4><br/>
+
+            <span 
+                id='diagramSpan' 
+                style={{display: 'block', width: '100%', height: '50%', overfolow: 'scroll'}}>
+                    <svg id='tree'></svg>
+                        <table id='hoffman-table'></table><br/> 
+                            <p>The average length of a symbol in the Huffman code is <span id='avgLength'>0</span> bits.</p>
+                            <p>The average length of a symbol in the equal-length code is <span id='bitsInEqualCode'>0</span>bits.</p>
+                            <p>The entropy of the input string is <span id='entropy'>0</span>bits.</p>
+            </span>
         </div>
     )
 }
